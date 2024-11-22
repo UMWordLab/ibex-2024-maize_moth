a3941f86a18c7379f830b69bf7e6ac0e0d3fcca3
Bianca Brie <briebi@umich.edu>
2024-11-21 22:39:25 -0500

    debugging from lab meeting today

--- data_includes/template.js
+++ data_includes/template.js
@@ -1,20 +1,15 @@
 PennController.ResetPrefix(null); // makes commmand names simpler
 
 // template to load experimental stimuli from CSV file
-Template("gulordava_amaze.csv", row => {
-    // sssign group based on a Latin Square design (alternating between 'list1' and 'list2')
-    var group = row.group; // ese the group column directly from csv (list1 or list2)
-
-    // generate the trial
-    return newTrial([row.label, row.num], "PennController", newTrial(
+Template("gulordava_amaze.csv", row => newTrial(row.label + _ + row.item, "PennController", newTrial(
         newController("Maze", {s: row.sentence, a: row.alternative, redo: true, time: 1000, emess: "Incorrect. Please wait..."})
             .print()
             .log()
             .wait()
     )
-    .log("counter", __counter_value_from_server__)
-    .log("num", row.num)               
+    .log("counter", __counter_value_from_server__)   
+    .log("item", row.item)         
     .log("label", row.label)     
-    .log("group", group) // Log the assigned group
-    );
-});
+    .log("group", row.group) // Log the assigned group
+    )
+)
